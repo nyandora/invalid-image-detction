745526970519d579f457e98ed8d95265d46c6e38
nyandora <goodingmorning@gmail.com>
2020-05-04 17:19:23 +0900

    refactoring.

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 
 const sharp = require('sharp')
 
-const check_valid_image = file_path => sharp(file_path).toBuffer()
+const check_image_validity = file_path => sharp(file_path).toBuffer()
 
-const image_file_names = ['normal.jpg', 
+const image_file_names = ['valid.jpg', 
                           'invalid_magic_number.jpg', // [Error: Input file contains unsupported image format]
                           'jpeg_magic_number_but_corrupted.jpg'] //[Error: Input file has corrupt header: VipsJpeg: Corrupt JPEG data: 597 extraneous bytes before marker 0xd4 VipsJpeg: JPEG datastream contains no image]
 
 image_file_names.forEach(file_name => {
-  check_valid_image('input_images/' + file_name)
+  check_image_validity('input_images/' + file_name)
     .then(() => console.log('it is a valid image! file name:', file_name))
     .catch(err => console.log('invalid image detected. file name:', file_name, ' error:', err))
 })
