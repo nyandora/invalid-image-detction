7c5da64ef30b1e1e435d8871c1f6efce1ae448ad
nyandora <goodingmorning@gmail.com>
2020-05-04 16:02:45 +0900

    refactoring.

--- index.js
+++ index.js
@@ -2,24 +2,16 @@
 
 const sharp = require('sharp')
 
-const isValidJpeg = async filePath => {
-  try {
-    await sharp(filePath).toBuffer()
-  } catch(err) {
-    console.log(err)
-    return false
-  }
-  return true
-}
+const check_valid_image = file_path => sharp(file_path).toBuffer()
 
-const test_images = ['normal.jpg', 
-                      'invalid_magic_number.jpg', // [Error: Input file contains unsupported image format]
-                      'jpeg_magic_number_but_corrupted.jpg'] //[Error: Input file has corrupt header: VipsJpeg: Corrupt JPEG data: 597 extraneous bytes before marker 0xd4 VipsJpeg: JPEG datastream contains no image]
+const image_file_names = ['normal.jpg', 
+                          'invalid_magic_number.jpg', // [Error: Input file contains unsupported image format]
+                          'jpeg_magic_number_but_corrupted.jpg'] //[Error: Input file has corrupt header: VipsJpeg: Corrupt JPEG data: 597 extraneous bytes before marker 0xd4 VipsJpeg: JPEG datastream contains no image]
 
-test_images.forEach(fileName => {
-  isValidJpeg('input_images/' + fileName)
-    .then(result => console.log('fileName:', fileName, ', result:', result))
-    .catch(err => console.log(err))
+image_file_names.forEach(file_name => {
+  check_valid_image('input_images/' + file_name)
+    .then(() => console.log('it is a valid image! fileName:', file_name))
+    .catch(err => console.log('invalid image detected. fileName:', file_name, ' error:', err))
 })
 
 
